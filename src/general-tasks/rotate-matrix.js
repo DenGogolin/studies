2ee0367c2da34ec734fef032428d3a5007c7ffc0
Denis Gogolin <denis_gogolin@epam.com>
2018-10-26 14:08:54 +0200

    typo

--- src/general-tasks/rotate-matrix.js
+++ src/general-tasks/rotate-matrix.js
@@ -1,10 +1,10 @@
-module.exports = (matrix, cloclWise = true) => {
+module.exports = (matrix, clockWise = true) => {
 	const len = matrix.length;
 	const n = len - 1;
 	for (let i = 0; i < len / 2; i++) {
 		for (let j = i; j < len - i - 1; j++) {
 			const temp = matrix[i][j];
-			if (cloclWise) {
+			if (clockWise) {
 				matrix[i][j] = matrix[n - j][i];
 				matrix[n - j][i] = matrix[n - i][n - j];
 				matrix[n - i][n - j] = matrix[j][n - i];
